c3d717c5cd213b69289594e183dd9dad67580b7d
Ikbal AZIMARI TOURE <azimariiki007@gmail.com>
2023-01-02 15:08:22 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodoFormComponent } from './todo-form/todo-form.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoFormComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule
